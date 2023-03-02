eaf5197b9eb8784ab7c4f118b290f0190e78dfb8
gregoriomartocci <gregoriomartocci@gmail.com>
2023-03-01 23:53:35 -0300

    Category module Added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeOrmConfig from 'config/ormconfig';
 import { ArticleModule } from './articles/article.module';
+import { CategoryModule } from './categories/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), ArticleModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ArticleModule,
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
--- server/src/categories/category.module.ts
+++ server/src/categories/category.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryController } from './category.controlles';
 import { Category } from './category.entity';
+import { CategoryRepository } from './category.repository';
 import { CategoryService } from './category.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
-  providers: [CategoryService, ],
+  providers: [CategoryService, CategoryRepository],
 })
-export class CategoriesModule {}
+export class CategoryModule {}
